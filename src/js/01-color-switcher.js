85fc62068f4c921fa00972f03dce047d9465d304
Igor432 <igomar0602@gmail.com>
2022-07-06 12:24:19 +0300

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const body = document.querySelector('body');
+const startBut = document.querySelector('[data-start]');
+const stopBut = document.querySelector('[data-stop]');
+let colorInterval = null;
+
+const onClick = () => {
+    colorInterval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    startBut.disabled = true;
+};
+
+const onStop = () => {
+    body.style.backgroundColor = 'white';
+    clearInterval(colorInterval);
+    startBut.disabled = false;
+}
+
+startBut.addEventListener('click', onClick);
+stopBut.addEventListener('click', onStop);
+
+
+
+
 
